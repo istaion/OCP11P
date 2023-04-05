bb1b5bb996fb2dfeb65cfe6f57f65d090cf8c037
istaion <v.poutot@gmail.com>
2023-04-05 16:16:15 +0200

    ajout de mocker pour la data. Je galère

--- tests/config.py
+++ tests/config.py
@@ -1,9 +1,57 @@
 import pytest
-from server import app
+import server
 
 @pytest.fixture
 def client():
     """ Allows the testing to be launched and under the Testing parameter"""
-    client = app.test_client()
+    client = server.app.test_client()
     return client
 
+
+def mock_load_competitions():
+    competitions = [
+        {
+            "name": "Spring Festival",
+            "date": "2025-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        },
+        {
+            "name": "test place",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "10"
+        }
+    ]
+    return competitions
+
+
+def mock_load_clubs():
+    clubs = [
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "13"
+        },
+        {
+            "name": "Iron Temple",
+            "email": "admin@irontemple.com",
+            "points": "4"
+        },
+        {
+            "name": "She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points": "500"
+        }
+    ]
+    return clubs
+
+
+@pytest.fixture
+def mock_data(mocker):
+    mocker.patch.object(server, 'competitions', mock_load_competitions())
+    mocker.patch.object(server, 'clubs', mock_load_clubs())
+    return mocker
--- tests/unitaires/test_unit_purchase.py
+++ tests/unitaires/test_unit_purchase.py
@@ -1,24 +1,26 @@
-from tests.config import client
+from tests.config import client, mock_data
 import pytest
 
 
 @pytest.mark.usefixtures("client")
 class Tests:
-    def test_purchase_places(self, client):
+    def test_purchase_places(self, client, mock_data):
         response = client.post('/purchasePlaces', data={'club': 'Simply Lift',
                                                         'competition': 'Spring Festival', 'places': '1'
                                                         }
                                )
         assert response.status_code == 200
         assert 'Great-booking complete!' in response.data.decode()
+        club = [c for c in clubs if c['name'] == 'Simply Lift'][0]
+        assert club['points'] == '12'
 
-    def test_purchase_places_past_competition(self, client):
+    def test_purchase_places_past_competition(self, client, mock_data):
         response = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Fall Classic',
-                                                        'places': 1})
+                                                        'places': '1'})
         assert response.status_code == 200
         assert 'book for a past competition' in response.data.decode()
 
-    def test_purchase_places_more_than_12(self, client):
+    def test_purchase_places_more_than_12(self, client, mock_data):
         response = client.post('/purchasePlaces', data={'club': 'Simply Lift',
                                                         'competition': 'Spring Festival', 'places': '13'
                                                         }
@@ -26,7 +28,7 @@ class Tests:
         assert response.status_code == 200
         assert 'book more than 12 points' in response.data.decode()
 
-    def test_purchase_places_not_enough_points(self, client):
+    def test_purchase_places_not_enough_points(self, client, mock_data):
         response = client.post('/purchasePlaces', data={'club': 'Iron Temple',
                                                         'competition': 'Spring Festival',
                                                         'places': '7'
@@ -35,7 +37,7 @@ class Tests:
         assert response.status_code == 200
         assert 'have enough points' in response.data.decode()
 
-    def test_purchase_places_not_enough_place(self, client):
+    def test_purchase_places_not_enough_place(self, client, mock_data):
         response = client.post('/purchasePlaces', data={'club': 'Iron Temple',
                                                         'competition': 'Spring Festival',
                                                         'places': '7'
